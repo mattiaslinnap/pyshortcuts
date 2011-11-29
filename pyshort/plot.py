7b5434637bc9a29b7a7994184ea317514e2fa50d
Mattias Linnap <mattias@linnap.com>
2011-11-29 17:50:16 +0000

    Some more comments

--- pyshort/plot.py
+++ pyshort/plot.py
@@ -8,11 +8,13 @@ from matplotlib import pyplot as pp
 import sys
 
 def close_figure_on_key(figure):
+    """Attaches keyboard events to quit to the figure. Clicking X is annoying.""" 
     def on_key(event):
         if event.key in ['escape', 'q']:
             sys.exit()
     figure.canvas.mpl_connect('key_release_event', on_key)
 
 def vertical_x_labels(axes):
+    """Makes x labels vertical. Useful if they are long strings."""
     for tl in axes.get_xticklabels(): 
         tl.set_rotation(-90)
