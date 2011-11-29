ff71e74efe4d37fb21a19488f265ba30abaf3c27
Mattias Linnap <mattias@linnap.com>
2011-11-29 17:49:03 +0000

    Matplotlib helpers

--- pyshort/plot.py
+++ pyshort/plot.py
@@ -4,3 +4,15 @@ Utilities for matplotlib.
 '''
 from __future__ import division
 
+from matplotlib import pyplot as pp
+import sys
+
+def close_figure_on_key(figure):
+    def on_key(event):
+        if event.key in ['escape', 'q']:
+            sys.exit()
+    figure.canvas.mpl_connect('key_release_event', on_key)
+
+def vertical_x_labels(axes):
+    for tl in axes.get_xticklabels(): 
+        tl.set_rotation(-90)
