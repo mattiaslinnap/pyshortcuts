32f18530ba8a10c388af1c4b03befe3c2dcbfb13
Mattias Linnap <mattias@linnap.com>
2013-02-15 22:28:03 +0000

    Removed GeoModelAdmin, as its no longer used but requires installing many PostGIS modules

--- pyshort/djangoshortcuts/admin.py
+++ pyshort/djangoshortcuts/admin.py
@@ -1,8 +1,11 @@
-from django.contrib.gis import admin
-from django.contrib.gis.geos import Point
+# from django.contrib.gis import admin
+# from django.contrib.gis.geos import Point
+from django.contrib import admin
+
 
 def permission_denied(self, request, obj=None):
     return False
+
 
 def readonly_admin(model, model_admin_base=admin.ModelAdmin):
     return type('ReadOnly%sAdmin' % model.__name__,
@@ -11,8 +14,8 @@ def readonly_admin(model, model_admin_base=admin.ModelAdmin):
                  'has_add_permission': permission_denied,
                  'has_delete_permission': permission_denied})
 
-
-class CambridgeOsmGeoAdmin(admin.OSMGeoAdmin):
-    # OSMGeoAdmin uses "Google" projection.
-    default_lon, default_lat = Point(0.1145, 52.2032, srid=4326).transform(900913, clone=True).coords
-    default_zoom = 13
+#
+# class CambridgeOsmGeoAdmin(admin.OSMGeoAdmin):
+#     # OSMGeoAdmin uses "Google" projection.
+#     default_lon, default_lat = Point(0.1145, 52.2032, srid=4326).transform(900913, clone=True).coords
+#     default_zoom = 13
