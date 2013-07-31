f5187ccc5640bc81d2aeccd8b568e524a33dc94b
Mattias Linnap <mattias@linnap.com>
2013-07-31 16:59:08 +0100

    Initial data parameter to deepdict.

--- pyshort/colls.py
+++ pyshort/colls.py
@@ -11,6 +11,8 @@ class deepdict(dict):
     {1: 10}
     >>> a = deepdict(); a[2][u'foo'][u'bar'] = 20; a
     {2: {u'foo': {u'bar': 20}}}
+    >>> a = deepdict({2: {u'foo': {u'baz': 30}}}); a[2][u'foo'][u'bar'] = 20; a
+    {2: {u'foo': {u'bar': 20, u'baz': 30}}}
 
     To catch bugs, overwriting a generated dict is not allowed:
     >>> a = deepdict(); a[1][u'foo'] = 10; a[1] = 20
@@ -18,6 +20,15 @@ class deepdict(dict):
     ...
     ValueError: Refusing to overwrite existing dictionary.
     """
+
+    def __init__(self, initial=None):
+        """If initial dictionary is given, it is recursively converted into deepdicts."""
+        if initial is not None:
+            for key in initial:
+                if isinstance(initial[key], dict):
+                    self[key] = deepdict(initial[key])
+                else:
+                    self[key] = initial[key]
 
     def __missing__(self, key):
         self[key] = deepdict()
@@ -28,6 +39,9 @@ class deepdict(dict):
             raise ValueError('Refusing to overwrite existing dictionary.')
         return super(deepdict, self).__setitem__(key, value)
 
+    def __getattr__(self, name):
+        """Only used if the attribute does not otherwise exist on the object."""
+        return self[name]
 
 if __name__ == '__main__':
     import doctest
