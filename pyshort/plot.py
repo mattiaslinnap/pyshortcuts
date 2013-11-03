f9a2e25613dca5f059cddeb050b16b6b99d916f0
Mattias Linnap <mattias@linnap.com>
2013-11-03 16:39:14 +0000

    Vertically compressed question to answer delay histogram.

--- pyshort/plot.py
+++ pyshort/plot.py
@@ -54,7 +54,7 @@ def rc_print_settings():
     matplotlib.rc('savefig', dpi=300, bbox='tight', format='pdf')
 
 
-def hist_fixed_bins(axes, data, bins, log=False, open_end=False, normed=False):
+def hist_fixed_bins(axes, data, bins, log=False, open_end=False, normed=False, color='b', alpha=1.0, label=None, ticklabels=None):
     """Plots a histogram with given bins, but equal plotted bin widths.
     Useful if bin widths are very unequal.
     open_end=True appends a bin edge at +infinity.
@@ -67,9 +67,9 @@ def hist_fixed_bins(axes, data, bins, log=False, open_end=False, normed=False):
     if normed:
         hist = np.array(hist, dtype=np.float64)
         hist /= len(data)
-    axes.bar(range(len(hist)), hist, width=0.99, log=log)
+    axes.bar(range(len(hist)), hist, width=0.99, log=log, color=color, alpha=alpha, label=label)
     axes.set_xticks(range(len(bins)))
-    axes.set_xticklabels([str(b) for b in bins])
+    axes.set_xticklabels(ticklabels if ticklabels else [str(b) for b in bins])
 
 
 def set_color_cycle_from_cmap(axes, cmap_name='spectral', num_colors=10, repeat_each=1):
