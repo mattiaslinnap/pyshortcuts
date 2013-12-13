c6d51bf6aa85272a9a06136b79cdb1bdbbba4187
Mattias Linnap <mattias@linnap.com>
2013-12-13 17:12:11 +0200

    Color-coded python strings for fancy terminal output

--- pyshort/strings.py
+++ pyshort/strings.py
@@ -5,13 +5,27 @@ from future_builtins import *  # ascii, filter, hex, map, oct, zip
 import cStringIO
 import gzip
 
+
 def printf(fmt=None, *args, **kwargs):
     if fmt:
         print(fmt.format(*args, **kwargs))
     else:
         print()
 
+
 def gunzip(gzdata):
     """Uncompresses gzip file data given as a string. The data is assumed to contain full gzip headers."""
     with gzip.GzipFile(mode='rb', fileobj=cStringIO.StringIO(gzdata)) as f:
         return f.read()
+
+
+def color(text, col='r'):
+    colorcode = {'r': '1;31',  # light red
+                 'b': '1;34',  # light blue
+                 'g': '1;32',  # light green
+                 'c': '1;36',  # light cyan
+                 'y': '1;33',  # yellow
+                 'm': '1;35',  # light purple
+                 'w': '1;37'   # white
+                 }[col]
+    return '\033[%sm%s\033[0m' % (colorcode, text)
